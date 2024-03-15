f2ed9a67668ef81ab829753c9faf224c12d4eb50
NPCRUS <npcrus@gmail.com>
2024-03-15 15:56:54 +0100

    small frontend impl

--- frontend/src/main/scala/App.scala
+++ frontend/src/main/scala/App.scala
@@ -1,10 +1,27 @@
-import com.raquo.laminar.api.{*, given}
 import com.raquo.laminar.api.L.*
 import org.scalajs.dom
 object App {
 
+  val gameId: Var[String] = Var("")
+  val response: Var[String] = Var("")
+
   val app: Element =
-    div(h1("Sosi"))
+    div(
+      "Put game id: ",
+      input(
+        value <-- gameId,
+        onInput.mapToValue --> gameId.writer
+      ),
+      button(
+        "Search",
+        onClick.flatMap { _ =>
+          FetchStream.get(s"http://localhost:8080/games/${gameId.now()}")
+        } --> (r => response.set(r))
+      ),
+      div(
+        child.text <-- response
+      )
+    )
 
   def main(args: Array[String]): Unit =
     render(dom.document.getElementById("app"), app)
